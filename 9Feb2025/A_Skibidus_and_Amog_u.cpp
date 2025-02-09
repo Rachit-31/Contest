d48a1f8eb5e1a20304f0a3cbd7ab3b80c93f698a
Rachit Jain <rachitjainjaipur@gmail.com>
2025-02-09 20:17:46 +0530

    done

---
+++ 9Feb2025/A_Skibidus_and_Amog_u.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h> 
+using namespace std;     
+
+#define int long long        
+#define nl <<'\n'           
+
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)a.size()
+#define pii pair<int, int>
+#define vi vector<int>
+#define mi map<int, int>
+#define umi unordered_map<int, int>
+#define si set<int>
+#define sc set<char>
+
+// functions
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define cf(i,s,e) for(int i=s;i<=e;i++)
+#define rf(i,e,s) for(int i=e-1;i>=s;i--)
+#define pb push_back
+#define in cin >>
+#define out cout <<
+
+const int MOD = 1e9 + 7;      
+const int INF = 1e18;  
+
+int gcd(int a,int b) { if (b==0) return a; return gcd(b, a%b); }
+int lcm(int a,int b) { return a/gcd(a,b)*b; }
+
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+void solve() {
+    string s;
+    in s;
+
+    int n = s.length();
+
+    if(s == "us"){
+        out "i" nl;
+        return;
+    }
+    int l = n-2;
+    string temp = s.substr(0,l);
+    
+
+    out temp + "i" nl;
+
+}
+
+int32_t main() {
+    fast_io();
+    int t;  
+    cin >> t;  
+    while (t--) {
+        solve(); 
+    }
+
+    return 0; 
+}
