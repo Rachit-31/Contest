80b5386725f4a51b79b54c2ad1dfb5b41c533111
Rachit Jain <rachitjainjaipur@gmail.com>
2025-03-11 20:11:02 +0530

    simple

---
+++ 11March2025/A_Draw_a_Square.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h> 
+using namespace std;     
+
+#define int long long        
+#define nl <<'\n'           
+
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)a.size()
+#define pii pair<int, int>
+#define vi vector<int>
+#define mi map<int, int>
+#define umi unordered_map<int, int>
+#define si set<int>
+#define sc set<char>
+
+// functions
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define cf(i,s,e) for(int i=s;i<=e;i++)
+#define rf(i,e,s) for(int i=e-1;i>=s;i--)
+#define pb push_back
+#define in cin >>
+#define out cout <<
+
+const int MOD = 1e9 + 7;      
+const int INF = 1e18;  
+
+int gcd(int a,int b) { if (b==0) return a; return gcd(b, a%b); }
+int lcm(int a,int b) { return a/gcd(a,b)*b; }
+
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+void solve() {
+    int l, r, d, u;
+    in l, in r, in d, in  u;
+
+    if(l==r && r==d && d==u){
+        yes();
+    }
+    else{
+        no();
+    }
+}
+
+int32_t main() {
+    fast_io();
+    int t;  
+    cin >> t;  
+    while (t--) {
+        solve(); 
+    }
+
+    return 0; 
+}
