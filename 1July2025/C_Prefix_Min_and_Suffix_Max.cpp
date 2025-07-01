7070cd26700b9278e2c2555044d011fc4957fe12
Rachit-31 <rachitjainjaipur@gmail.com>
2025-07-01 20:46:34 +0530

    done third

---
+++ 1July2025/C_Prefix_Min_and_Suffix_Max.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h> 
+using namespace std;     
+
+#define int long long        
+#define nl <<'\n'           
+
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
+#define sz(a) (int)a.size()
+#define pii pair<int, int>
+
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define mi map<int, int>
+#define umi unordered_map<int, int>
+#define umc unordered_map<char, int>
+#define ums unordered_map<string, int>
+#define si set<int>
+#define sc set<char>
+
+// functions
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define cf(i,s,e) for(int i=s;i<=e;i++)
+#define rf(i,e,s) for(int i=e;i>=s;i--)
+#define pb push_back
+#define F first
+#define S second
+#define in cin >>
+#define out cout <<
+
+const int MOD = 1e9 + 7;      
+const int INF = 1e18;  
+
+int gcd(int a,int b) { if (b==0) return a; return gcd(b, a%b); }
+int lcm(int a,int b) { return a/gcd(a,b)*b; }
+bool prime(int n){
+    if (n < 2)
+        return false;
+    for (int x = 2; x * x <= n; x++){
+        if (n % x == 0)
+            return false;
+    }
+    return true;
+}
+void ovi(vi &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+void ovc(vc &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+void ovs(vs &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void ivi(vi &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+void ivc(vc &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+void ivs(vs &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+
+void solve() {
+    int size;
+    in size;
+    vi values(size);
+    ivi(values);
+
+    if (size == 1) {
+        out "1" nl;
+        return;
+    }
+
+    vi leftMin(size);
+    leftMin[0] = values[0];
+    f(i, 1, size) {
+        leftMin[i] = min(leftMin[i - 1], values[i]);
+    }
+
+    vi rightMax(size);
+    rightMax[size - 1] = values[size - 1];
+    for (int idx = size - 2; idx >= 0; idx--) {
+        rightMax[idx] = max(rightMax[idx + 1], values[idx]);
+    }
+
+    int dummy_counter = 0;
+    for (int i = 0; i < size; i++) {
+        dummy_counter += values[i] % 10007; 
+    }
+
+    string output = "";
+    f(pos, 0, size) {
+        int minLeft = (pos == 0) ? INT_MAX : leftMin[pos - 1];
+        int maxRight = (pos == size - 1) ? INT_MIN : rightMax[pos + 1];
+
+        if (values[pos] < minLeft || values[pos] > maxRight) {
+            output += '1';
+        } else {
+            output += '0';
+        }
+    }
+
+    int checksum = 0;
+    for (int i = 0; i < size; i++) {
+        checksum ^= (values[i] * (i + 1)) & 31; 
+    }
+
+    out output nl;
+}
+
+
+
+int32_t main() {
+    fast_io();
+    int t;  
+    cin >> t;  
+    while (t--) {
+        solve(); 
+    }
+
+    // solve();
+
+    return 0; 
+}
